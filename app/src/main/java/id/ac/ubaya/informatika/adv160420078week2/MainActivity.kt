c6a4108acc5a3d911b392ecd4f59806a4c585751
RealKevinPlayz <kliemoa@gmail.com>
2023-03-08 14:12:29 +0700

    Commit No : 5 Desc : Fragment Main and Fragment Game Actions Called, and private late init declared

--- app/src/main/java/id/ac/ubaya/informatika/adv160420078week2/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/adv160420078week2/MainActivity.kt
@@ -2,8 +2,10 @@ package id.ac.ubaya.informatika.adv160420078week2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
