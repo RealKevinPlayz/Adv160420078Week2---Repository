f985bcd7ead9b0466a55d3bbfebaa5d120fcc0b9
RealKevinPlayz <kliemoa@gmail.com>
2023-03-08 16:16:41 +0700

    Commit No : 8 Desc : Result Fragment created, Game Fragment UI Edited, Now Game Fragment have the ability to play an Addition Game, Each correct answer gives you 1 Score, Incorrect answer lead you to game over screen.
    answer input now erased its content after correct answer

--- app/src/main/java/id/ac/ubaya/informatika/adv160420078week2/GameFragment.kt
+++ app/src/main/java/id/ac/ubaya/informatika/adv160420078week2/GameFragment.kt
@@ -1,10 +1,12 @@
 package id.ac.ubaya.informatika.adv160420078week2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_game.*
 
@@ -17,15 +19,41 @@ class GameFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_game, container, false)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        var score = 0
+        var answer = 0
+
+        var randomX = (0..1000).random()
+        var randomY = (0..1000).random()
+
+        answer = randomX + randomY
+
+        if(answer != null){
+            txtQuestion.text = "$randomX + $randomY"
+        }
+
+
         super.onViewCreated(view, savedInstanceState)
         if(arguments != null) {
             val playerName =
                 GameFragmentArgs.fromBundle(requireArguments()).playerName
             txtTurn.text = "$playerName's Turn"
         }
-        btnBack.setOnClickListener {
-            val action = GameFragmentDirections.actionMainFragment()
-            Navigation.findNavController(it).navigate(action)
+        btnSubmit.setOnClickListener {
+            var playerAnswer = txtAnswer.text
+            if(playerAnswer.toString() == answer.toString()){
+                score++
+
+                randomX = (0..1000).random()
+                randomY = (0..1000).random()
+
+                answer = randomX + randomY
+                txtQuestion.text = "$randomX + $randomY"
+                Toast.makeText(activity, "Well Done, You Answered Correctly", Toast.LENGTH_SHORT).show()
+                txtAnswer.setText("")
+            }else{
+                val action = GameFragmentDirections.actionResultFragment(score)
+                Navigation.findNavController(it).navigate(action)
+            }
         }
     }
 }
