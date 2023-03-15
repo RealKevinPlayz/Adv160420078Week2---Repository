b6b99455fd418d8ae6e14012e8200b8917298994
RealKevinPlayz <kliemoa@gmail.com>
2023-03-15 14:15:47 +0700

    Branch : Week3 Commit No : 4 Desc : game_navigation fragments renamed, MainActivity.kt nav controller and bottomNav. bottomNav setupWithNavController added

--- app/src/main/java/id/ac/ubaya/informatika/adv160420078week2/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/adv160420078week2/MainActivity.kt
@@ -5,16 +5,22 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        navController =
-//            (supportFragmentManager.findFragmentById(R.id.actionGameFragment) as
-//                    NavHostFragment).navController
-//        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.HostContainer)
+                as NavHostFragment).navController
+
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+
+        bottomNav.setupWithNavController(navController)
     }
 
 }
