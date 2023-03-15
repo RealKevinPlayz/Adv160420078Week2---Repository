6fc057bcbc881a7af56100bf17e96e2ce798dc54
RealKevinPlayz <kliemoa@gmail.com>
2023-03-15 14:59:02 +0700

    Branch : Week3 Commit No : 7 Desc : Navigation Drawer xml code added. navView added. navView connection established

--- app/src/main/java/id/ac/ubaya/informatika/adv160420078week2/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/adv160420078week2/MainActivity.kt
@@ -2,6 +2,7 @@ package id.ac.ubaya.informatika.adv160420078week2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -11,16 +12,26 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        drawerLayout = findViewById(R.id.drawerLayout)
+
         navController = (supportFragmentManager.findFragmentById(R.id.HostContainer)
                 as NavHostFragment).navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navView, navController)
 
         var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
 
         bottomNav.setupWithNavController(navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout)
+                || super.onSupportNavigateUp()
     }
 
 }
